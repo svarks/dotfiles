64f5d6e781ca12ea7b587deb086a8ec439b9eb97
Sergey Varaksin <varaksin86@gmail.com>
2012-07-28 19:19:47 -0500

    homebrew tmp dir

--- shell/env.sh
+++ shell/env.sh
@@ -4,6 +4,10 @@ fi
 export PATH=/usr/local/bin:${ORIGINAL_PATH/\:\/usr\/local\/bin/}
 export EDITOR=vim
 
+if [[ -x $HOME/homebrew ]]; then
+  export HOMEBREW_TEMP=$HOME/homebrew/tmp
+fi
+
 if which rbenv > /dev/null; then
   eval "$(rbenv init -)"
 fi
